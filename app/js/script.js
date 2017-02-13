3f7a1e5a6a021bb63de7d65f0f86b1d4f9ccb641
DelaiHorosho <Rik256@yandex.ru>
2017-02-13 16:15:52 +0300

    lombard full

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function(){
    $(".burger").click(function () {
        $(this).toggleClass("menu-on");
-       $('ul#menu-verhnee-menyu').toggleClass("menu-on");
+       $('.header__navigation').toggleClass("menu-on"); 
     });
 });
